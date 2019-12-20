4d15e8ae0ae05302015c410e65c3fc0ef2979f22
Pluritechno <33075121+Pluritechno@users.noreply.github.com>
2019-12-19 21:04:26 -0500

    Add files via upload

---
+++ Apache Benchmark Peticiones/src/Logica/ClaseP.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logica;
+
+/**
+ *
+ * @author osboxes
+ */
+
+import Vista.Interfaz;
+import java.io. IOException;
+
+public class ClaseP {
+    
+public static void main(String[] args) throws IOException, InterruptedException  {
+
+    Interfaz vista = new Interfaz ();
+    
+    vista.setLocationRelativeTo(null);
+    vista.setVisible(true);
+        
+}
+
+}
